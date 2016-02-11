e4e2830dcc75337030bad4d8efc5aa77ccafed17
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-02-11 00:58:39 -0500

    plup.fish: handle relative path in new Plover

--- config/fish/functions/plup.fish
+++ config/fish/functions/plup.fish
@@ -1,6 +1,17 @@
 function plup --description 'Searches for words in all configured Plover dictionaries.'
-	set -l config "$HOME/Library/Application Support/plover/plover.cfg"
+	set -l base "$HOME/Library/Application Support/plover"
+	set -l config "$base/plover.cfg"
     set -l dict_files (awk 'BEGIN{FS=" = "} /^dictionary_file/{print($2)}' $config)
+
+    # Recent Plover builds use a path relative to $config, at least for
+    # sibling files.
+    for i in (seq (count $dict_files))
+        set -l fnam $dict_files[$i]
+        if test (dirname $fnam) = .
+            set dict_files[$i] "$base/$fnam"
+        end
+    end
+
     for word in $argv
         egrep -i $word -- $dict_files
     end
