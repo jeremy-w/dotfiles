fbe29363834bebda75bca2fa4caec4dd45c0fcd4
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-08-28 12:02:35 -0400

    VSCODE/_install: Explain how to run this

--- _VSCode/_install.sh
+++ _VSCode/_install.sh
@@ -1,3 +1,5 @@
+# NOTE: This expects to be run in this directory!
+# Install and launch VSCode once before running this.
 set VSCODE_SETTINGS ~/Library/Application\ Support/Code/User/
 
 # Initial import - already done
@@ -6,6 +8,12 @@ set VSCODE_SETTINGS ~/Library/Application\ Support/Code/User/
 #mv $VSCODE_SETTINGS/settings.json .
 
 # Linking these into place where VSCode expects them
+set base (dirname $0)
+
+# New VSCode creates an empty snippets folder.
+rmdir $VSCODE_SETTINGS/snippets
 ln -s $PWD/snippets/ $VSCODE_SETTINGS/snippets
+
+# It doesn't create keybindings or settings, though.
 ln -s $PWD/keybindings.json $VSCODE_SETTINGS/keybindings.json
 ln -s $PWD/settings.json $VSCODE_SETTINGS/settings.json
