74f6823b3d342bd1135086f952edab82f26bded8
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-07-08 08:48:24 -0400

    Skip install-dotfiles itself and underscore-prefixed files

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -6,7 +6,13 @@ error_count=0
 function link_contents_of {
   file="$1"
   name=$(basename "$file")
-  # TODO: If name starts with underscore, log and skip!
+
+  # If name starts with underscore, log and skip.
+  if [[ $name =~ ^_  ]]; then
+    echo "$0: info: skipping $file because it starts with an underscore. Further action might be needed to install it."
+    return
+  fi
+
   target=".$name"
   # Avoid putting a directory link inside an extant directory.
   if test -L "$target"; then
@@ -25,7 +31,7 @@ function link_contents_of {
 }
 
 for file in dotfiles/*; do
-  link_contents_of "$file"
+  [[ $name != install-dotfiles.sh ]] && link_contents_of "$file"
 done
 
 # Handle .ssh specially.
@@ -33,5 +39,4 @@ install -d .ssh
 chmod 0700 .ssh
 mv .ssh,config .ssh/config
 
-rm .install-dotfiles.sh
 echo "$0: dotfiles installed: $warn_count warnings, $error_count errors"
