c7c25923eb2945f2aa017eddf421e5b659d5b03e
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-07-08 08:48:08 -0400

    Switch install-dotfiles.sh to bash for shellcheck

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,27 +1,26 @@
-#!/bin/zsh
+#!/bin/bash
 # Copy this up a directory, then run it.
 warn_count=0
 error_count=0
 
 function link_contents_of {
   file="$1"
-  name="`basename $file`"
+  name=$(basename "$file")
   # TODO: If name starts with underscore, log and skip!
   target=".$name"
   # Avoid putting a directory link inside an extant directory.
   if test -L "$target"; then
     echo "$0: warning: $target: already a symlink - skipping" 1>&2
-    warn_count=`expr $warn_count + 1`
+    warn_count=$((warn_count + 1))
   elif test -d "$target"; then
     # FIXME: .config very likely already exists.
     # We need a better way to handle this.
     echo "$0: error: $target: directory exists - skipping" 1>&2
-    error_count=`expr $error_count + 1`
+    error_count=$((error_count + 1))
+  elif ln -s "$PWD/$file" "$target"; then
+    return
   else
-    if ln -s "$PWD/$file" "$target"; then
-    else
-        error_count=`expr $error_count + 1`
-    fi
+    error_count=$((error_count + 1))
   fi
 }
 
