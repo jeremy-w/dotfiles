34b215a3ccd7ec3d529a795d0358e69e16b200e3
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2022-07-26 15:55:08 -0400

    fish: Alias brew to ARM, ibrew to Intel installs

---
+++ config/fish/functions/ibrew.fish
@@ -0,0 +1,3 @@
+function ibrew --wraps='arch -x86-64 brew' --wraps='arch -x86_64 /usr/local/bin/brew' --description 'alias ibrew arch -x86_64 /usr/local/bin/brew'
+  arch -x86_64 /usr/local/bin/brew $argv; 
+end
