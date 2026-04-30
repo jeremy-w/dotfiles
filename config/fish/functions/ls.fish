b6982e8a21285f909b68be1cab49f4402e23493f
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2026-04-30 16:42:06 -0400

    fish: ls: prefer eza when available

--- config/fish/functions/ls.fish
+++ config/fish/functions/ls.fish
@@ -1,4 +1,14 @@
 # Defined in /var/folders/cd/hfx1wsqd23j4ht0g532jdt4w0000gp/T//fish.RLH3fC/ls.fish @ line 2
 function ls --description 'alias ls ls -FG@eh,'
- command ls -FG@eh, $argv; 
+ if command -q eza
+   # eza -F can take a "when" argument, so needs to be separate.
+   # -@ is too verbose to use always, showing the bytes for the keys.
+   eza --header --classify=auto $argv
+ else
+    # Not in the eza list:
+    # -, to include thousands separators - unneeded with -h.
+    # -e to include ACLs with -l output (does nothing unless -l passed)
+    # eza lacks ACL support as of 2026-04-11.
+    command ls -FG@eh, $argv;
+ end
 end
