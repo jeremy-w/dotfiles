37a2cb6fd3089855c77c1135d85c806dbc27f6e4
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2026-06-18 12:26:11 -0400

    fish: casa-pgcli: set search path with init command

--- config/fish/functions/casa-pgcli.fish
+++ config/fish/functions/casa-pgcli.fish
@@ -1,3 +1,3 @@
 function casa-pgcli
-    cd ~/git/wintriad/casa/core-api/ && uvx --from python-dotenv[cli] dotenv run pgcli
+    cd ~/git/wintriad/casa/core-api/ && uvx --from python-dotenv[cli] dotenv run pgcli --init-command 'set search_path to propco_portal, public;'
 end
