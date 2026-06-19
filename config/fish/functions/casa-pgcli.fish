56f22b8ec2c649fb16b737d0f7000459ee7eec84
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2026-06-19 10:09:06 -0400

    fish(casa-pgcli): switch uvx dotenv to bash source, set search_path

--- config/fish/functions/casa-pgcli.fish
+++ config/fish/functions/casa-pgcli.fish
@@ -1,3 +1,3 @@
 function casa-pgcli
-    cd ~/git/wintriad/casa/core-api/ && uvx --from python-dotenv[cli] dotenv run pgcli --init-command 'set search_path to propco_portal, public;'
+    cd ~/git/wintriad/casa/core-api/ && /usr/bin/env bash -c 'set -a; source .env; set +a; pgcli --init-command "set search_path to propco_portal, public;"'
 end
