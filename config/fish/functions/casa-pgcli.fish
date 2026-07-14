18ac2578b5469cac5a5cb6a73588c9f11a308ae8
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2026-07-14 09:59:51 -0400

    casa-pgcli: fix issue where it left the directory changed after running

--- config/fish/functions/casa-pgcli.fish
+++ config/fish/functions/casa-pgcli.fish
@@ -1,3 +1,3 @@
 function casa-pgcli
-    cd ~/git/wintriad/casa/core-api/ && /usr/bin/env bash -c 'set -a; source .env; set +a; pgcli --init-command "set search_path to propco_portal, public;"'
+    /usr/bin/env bash -c 'set -a; source ~/git/wintriad/casa/core-api/.env; set +a; pgcli --init-command "set search_path to propco_portal, public;"'
 end
